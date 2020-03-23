313fb9bc69d80040bc55813fa393f9d86feb9b7d
Alison Vermeil <alison.minton@gmail.com>
2020-03-23 16:20:01 -0600

    Add relationship assertions to reading collection

--- spec/models/reading_collection_spec.rb
+++ spec/models/reading_collection_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe ReadingCollection, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :user_id }
   end
+
+  describe "relationships" do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to have_many :reading_collection_articles }
+    it { is_expected.to have_many(:articles).through(:reading_collection_articles) }
+  end
 end
