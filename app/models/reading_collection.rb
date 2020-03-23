afe713f80d3e02ce17a03cd660319c70f75296aa
Alison Vermeil <alison.minton@gmail.com>
2020-03-23 16:20:35 -0600

    Add relationships to reading_collection model

--- app/models/reading_collection.rb
+++ app/models/reading_collection.rb
@@ -1,5 +1,7 @@
 class ReadingCollection < ApplicationRecord
   belongs_to :user
+  has_many :reading_collection_articles
+  has_many :articles, through: :reading_collection_articles
 
   validates :name, :user_id, presence: true
 end
