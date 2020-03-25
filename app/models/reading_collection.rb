d9aa60fd1a6e1bc047fc95d2a1269ad6aa994f35
Phil DeLong <philjdelong@gmail.com>
2020-03-25 00:09:47 -0600

    Update relationships

--- app/models/reading_collection.rb
+++ app/models/reading_collection.rb
@@ -9,17 +9,6 @@ class ReadingCollection < ApplicationRecord
   private
 
   def set_slug
-    # same way they are setting slugs elsewhere in the app
     self.slug = name.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
-  end
-
-  has_many :reading_collection_tags
-  has_many :tags, through: :reading_collection_tags
-  validates :name, :user_id, presence: true
-
-  def get_articles(tags)
-    self.articles = tags.map do |t|
-      Article.cached_tagged_with(t).first
-    end.to_a
   end
 end
