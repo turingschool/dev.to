1524a1788402f87b8abf2ba876bd09911739209a
Phil DeLong <philjdelong@gmail.com>
2020-03-25 00:06:54 -0600

    Create reading collections controller

--- app/controllers/reading_collections_controller.rb
+++ app/controllers/reading_collections_controller.rb
@@ -2,4 +2,21 @@ class ReadingCollectionsController < ApplicationController
   def show
     @collection = ReadingCollection.find_by(slug: params[:slug])
   end
+
+  def new
+    # uncomment later
+  end
+
+  def create
+    ReadingCollection.create(collection_params)
+    redirect_to "/readinglist"
+  end
+
+  private
+
+  def collection_params
+    {
+      name: params[:name], user: current_user
+    }
+  end
 end
