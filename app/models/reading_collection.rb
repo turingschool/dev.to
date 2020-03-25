828fc8d3acf8ead29948d2739a87f754964a1fe0
Phil DeLong <philjdelong@gmail.com>
2020-03-25 00:09:47 -0600

    Update relationships

--- app/models/reading_collection.rb
+++ app/models/reading_collection.rb
@@ -2,10 +2,8 @@ class ReadingCollection < ApplicationRecord
   belongs_to :user
   has_many :reading_collection_articles, dependent: :destroy
   has_many :articles, through: :reading_collection_articles
-
   validates :name, :user_id, presence: { presence: true }
   validates :slug, presence: true, format: /\A[0-9a-z\-_]*\z/, uniqueness: { scope: :user_id }
-
   before_validation :set_slug
 
   private
@@ -14,4 +12,8 @@ class ReadingCollection < ApplicationRecord
     # same way they are setting slugs elsewhere in the app
     self.slug = name.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
   end
+
+  has_many :reading_collection_tags
+  has_many :tags, through: :reading_collection_tags
+  validates :name, :user_id, presence: true
 end
