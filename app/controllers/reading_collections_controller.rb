c568fe4b564073d4bf8970d7167546252c00131c
Phil DeLong <philjdelong@gmail.com>
2020-03-25 00:06:54 -0600

    Create reading collections controller

--- app/controllers/reading_collections_controller.rb
+++ app/controllers/reading_collections_controller.rb
@@ -2,37 +2,4 @@ class ReadingCollectionsController < ApplicationController
   def show
     @collection = ReadingCollection.find_by(slug: params[:slug])
   end
-
-  def new
-    # will complete when refactoring partial
-  end
-
-  def create
-    reading_collection = ReadingCollection.create(collection_params)
-    rc_tags = params["tags"]
-    reading_collection.get_articles(rc_tags)
-    redirect_to "/readinglist"
-  end
-
-  private
-
-  def collection_params
-    {
-      name: params[:name], user: current_user
-    }
-  end
 end
-
-# articles = Article.joins(:tags).where("tags.name = JavaScript").where("articles.created_at BETWEEN ? AND ?",Time.now-1.weeks,Time.now)
-
-# Timeframe values from Timeframer::DATETIMES
-# def top_articles_by_timeframe(timeframe:)
-#   published_articles_by_tag.where("published_at > ?", Timeframer.new(timeframe).datetime).
-#     order("score DESC").page(@page).per(@number_of_articles)
-# end
-
-# def latest_feed
-# published_articles_by_tag.order("published_at DESC").
-# where("featured_number > ? AND score > ?", 1_449_999_999, -40).
-# page(@page).per(@number_of_articles)
-# end
