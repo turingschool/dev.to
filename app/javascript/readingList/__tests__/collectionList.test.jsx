0866cca6e3694e41316b86119b14522824ddb295
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-24 22:32:33 -0600

    Update ReadingList and CollectionList to account for added collections prop

--- app/javascript/readingList/__tests__/collectionList.test.jsx
+++ app/javascript/readingList/__tests__/collectionList.test.jsx
@@ -4,7 +4,11 @@ import { CollectionList } from '../collectionList';
 
 describe('<ContentList />', () => {
   it('renders properly', () => {
-    const tree = render(<CollectionList />);
+    const mockCollections = [
+      { id: 1, name: 'first_test_post' },
+      { id: 2, name: 'second_test_post' },
+    ];
+    const tree = render(<CollectionList collections={mockCollections} />);
     expect(tree).toMatchSnapshot();
   });
 });
--- app/javascript/readingList/__tests__/readingList.test.jsx
+++ app/javascript/readingList/__tests__/readingList.test.jsx
@@ -4,7 +4,13 @@ import { ReadingList } from '../readingList';
 
 describe('<ReadingList />', () => {
   it('renders properly', () => {
-    const tree = render(<ReadingList availableTags={['discuss']} />);
+    const mockCollections = [
+      { id: 1, name: 'first_test_post' },
+      { id: 2, name: 'second_test_post' },
+    ];
+    const tree = render(
+      <ReadingList availableTags={['discuss']} collections={mockCollections} />,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
