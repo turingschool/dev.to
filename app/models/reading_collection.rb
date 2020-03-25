dacb6e823e33d74d388585148034bc058c209fc2
Phil DeLong <philjdelong@gmail.com>
2020-03-25 00:09:47 -0600

    Update relationships

--- app/models/reading_collection.rb
+++ app/models/reading_collection.rb
@@ -3,5 +3,8 @@ class ReadingCollection < ApplicationRecord
   has_many :reading_collection_articles, dependent: :destroy
   has_many :articles, through: :reading_collection_articles
 
+  has_many :reading_collection_tags
+  has_many :tags, through: :reading_collection_tags
+
   validates :name, :user_id, presence: true
 end
