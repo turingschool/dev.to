f493c1a19abc6b841ae46acee1ae6ab2ddb012e6
msimon42 <msimon@uab.edu>
2020-03-25 18:12:00 -0600

    Add mailer
    Co-Authored-By: Melissa Robbins <melissamrobbins@gmail.com>

--- app/mailers/daily_dev_mailer.rb
+++ app/mailers/daily_dev_mailer.rb
@@ -1,2 +1,11 @@
 class DailyDevMailer < ApplicationMailer
+  default from: -> { "Daily Dev <#{SiteConfig.default_site_email}>" }
+
+  def daily_dev_email(user, article)
+    @article = article
+    @user = user
+    @unsubscribe = generate_unsubscribe_token(@user.id, :email_daily_dev)
+    subject = "Your Daily Dev"
+    mail(to: @user.email, subject: subject)
+  end
 end
