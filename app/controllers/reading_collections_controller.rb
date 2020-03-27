9d5c3d996bac6de001c6d907d6ac7e7da46931a6
Phil DeLong <philjdelong@gmail.com>
2020-03-27 13:34:12 -0600

    Remove methods that were added to API

--- app/controllers/reading_collections_controller.rb
+++ app/controllers/reading_collections_controller.rb
@@ -1,20 +1,3 @@
 class ReadingCollectionsController < ApplicationController
-  def new
-    # will complete when refactoring partial
-  end
-
-  def create
-    reading_collection = ReadingCollection.create(collection_params)
-    rc_tags = params["tags"]
-    reading_collection.get_articles(rc_tags)
-    redirect_to "/readinglist"
-  end
-
-  private
-
-  def collection_params
-    { name: params[:name], user: current_user }
-  end
+  def new; end
 end
-
-# articles = Article.joins(:tags).where("tags.name = JavaScript").where("articles.created_at BETWEEN ? AND ?",Time.now-1.weeks,Time.now)
