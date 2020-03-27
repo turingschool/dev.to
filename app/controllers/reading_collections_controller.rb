ad871de5acca2b365ad67ff96c285149dbafba2e
Phil DeLong <philjdelong@gmail.com>
2020-03-27 10:58:19 -0600

    Remove comments

--- app/controllers/reading_collections_controller.rb
+++ app/controllers/reading_collections_controller.rb
@@ -18,15 +18,3 @@ class ReadingCollectionsController < ApplicationController
 end
 
 # articles = Article.joins(:tags).where("tags.name = JavaScript").where("articles.created_at BETWEEN ? AND ?",Time.now-1.weeks,Time.now)
-
-# Timeframe values from Timeframer::DATETIMES
-# def top_articles_by_timeframe(timeframe:)
-#   published_articles_by_tag.where("published_at > ?", Timeframer.new(timeframe).datetime).
-#     order("score DESC").page(@page).per(@number_of_articles)
-# end
-
-# def latest_feed
-# published_articles_by_tag.order("published_at DESC").
-# where("featured_number > ? AND score > ?", 1_449_999_999, -40).
-# page(@page).per(@number_of_articles)
-# end
