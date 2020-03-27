28e0e4690c280a19f9d3820098496c40d039deb7
Phil DeLong <philjdelong@gmail.com>
2020-03-27 13:33:49 -0600

    Create API controller

---
+++ app/controllers/api/v0/reading_collections_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V0
+    class ReadingCollectionsController < ApiController
+      def create
+        reading_collection = current_user.reading_collections.create(collection_params)
+        articles = reading_collection.get_articles
+        reading_collection.articles << articles
+        @reading_collection = reading_collection.to_json
+      end
+
+      private
+
+      def collection_params
+        params.permit(:name, tag_list: [])
+      end
+    end
+  end
+end
