9f60cbb929a43063d8f63f1b1f2aedcdcdb93698
Phil DeLong <philjdelong@gmail.com>
2020-03-26 22:27:14 -0600

    Update controller with create method

--- app/controllers/reading_collections_controller.rb
+++ app/controllers/reading_collections_controller.rb
@@ -4,11 +4,13 @@ class ReadingCollectionsController < ApplicationController
   end
 
   def new
-    # uncomment later
+    # will complete when refactoring partial
   end
 
   def create
-    ReadingCollection.create(collection_params)
+    reading_collection = ReadingCollection.create(collection_params)
+    rc_tags = params["tags"]
+    reading_collection.get_articles(rc_tags)
     redirect_to "/readinglist"
   end
 
@@ -20,3 +22,17 @@ class ReadingCollectionsController < ApplicationController
     }
   end
 end
+
+# articles = Article.joins(:tags).where("tags.name = JavaScript").where("articles.created_at BETWEEN ? AND ?",Time.now-1.weeks,Time.now)
+
+# Timeframe values from Timeframer::DATETIMES
+# def top_articles_by_timeframe(timeframe:)
+#   published_articles_by_tag.where("published_at > ?", Timeframer.new(timeframe).datetime).
+#     order("score DESC").page(@page).per(@number_of_articles)
+# end
+
+# def latest_feed
+# published_articles_by_tag.order("published_at DESC").
+# where("featured_number > ? AND score > ?", 1_449_999_999, -40).
+# page(@page).per(@number_of_articles)
+# end
