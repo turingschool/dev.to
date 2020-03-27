9cbc7e628c5f8132695ebed7c71c0ed20ac9c919
Alison Vermeil <alison.minton@gmail.com>
2020-03-26 20:42:22 -0600

    Add assertions to reading_collection show test

--- spec/system/reading_collections/user_visits_a_collection_spec.rb
+++ spec/system/reading_collections/user_visits_a_collection_spec.rb
@@ -5,16 +5,20 @@ RSpec.describe "Views a collection", type: :system do
   let_it_be(:article_1, reload: true) { create(:article, :with_notification_subscription, user: user) }
   let_it_be(:article_2, reload: true) { create(:article, :with_notification_subscription, user: user) }
   let_it_be(:article_3, reload: true) { create(:article, :with_notification_subscription, user: user) }
-  # let(:timestamp) { "2019-03-04T10:00:00Z" }
   let_it_be(:reading_collection) { create(:reading_collection, user: user) }
+  let_it_be(:reading_collection2) { create(:reading_collection, user: user) }
 
   before do
     user.reading_collections.first.articles << [article_1, article_2, article_3]
     sign_in user
-    # visit "/readingcollections/#{article.slug}"
+    visit "/readingcollections/#{reading_collection.slug}"
   end
 
-  it "shows an article" do
-    expect(page).to have_content(article.title)
+  it "shows a reading collection" do
+    expect(page).to have_content(reading_collection.name)
+    expect(page).not_to have_content(reading_collection2.name)
+  end
+
+  it "shows the articles associated with the collection" do
   end
 end
