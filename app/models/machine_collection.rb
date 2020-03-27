eec6861238ec8524f84db73eb1cdae7856c04d90
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-27 07:16:59 -0600

    Add random slug generator and have it run before validations

--- app/models/machine_collection.rb
+++ app/models/machine_collection.rb
@@ -1,4 +1,9 @@
 class MachineCollection < ApplicationRecord
-  validates :cached_tag_list, :slug, :title, presence: true
+  before_validation :create_slug
+  validates :cached_tag_list, :title, presence: true
   belongs_to :user
+
+  def create_slug
+    self.slug = title.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
+  end
 end
