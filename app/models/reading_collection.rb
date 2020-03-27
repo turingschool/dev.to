7f2627a0e6dae2b84dc9a20527be416dce89f52f
Phil DeLong <philjdelong@gmail.com>
2020-03-26 22:28:08 -0600

    Create method get_articles

--- app/models/reading_collection.rb
+++ app/models/reading_collection.rb
@@ -7,4 +7,10 @@ class ReadingCollection < ApplicationRecord
   has_many :tags, through: :reading_collection_tags
 
   validates :name, :user_id, presence: true
+
+  def get_articles(tags)
+    self.articles = tags.map do |t|
+      Article.cached_tagged_with(t).first
+    end.to_a
+  end
 end
