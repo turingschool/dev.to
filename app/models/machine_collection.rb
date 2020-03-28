93226c953fac4fa1fb0926e78b63bf949a69c034
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-28 11:50:43 -0600

    Create method to return collection of articles related to tag from past seven days

--- app/models/machine_collection.rb
+++ app/models/machine_collection.rb
@@ -6,4 +6,9 @@ class MachineCollection < ApplicationRecord
   def create_slug
     self.slug = title.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
   end
+
+  def articles_past_seven_days
+    Article.where("position('#{cached_tag_list}' in cached_tag_list) > 0").
+      where("created_at > '#{Time.zone.today - 7.days}'")
+  end
 end
