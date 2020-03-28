8e1ab89c6df82b1064f7ffba1e37cb643c76e6e1
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-28 09:29:25 -0600

    Add form validation and return linking on collectionForm

--- app/javascript/readingList/collectionForm.jsx
+++ app/javascript/readingList/collectionForm.jsx
@@ -21,56 +21,62 @@ export class CollectionForm extends Component {
   submitCollection = (e, title, tags) => {
     // check that tag length is > 1, title req handled on input
     // need to modify route also and ensure error handling is included.
+
+    // if successful route to readinglist               href="/readinglist"
     e.preventDefault();
-    fetch('/reading_collections', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'X-CSRF-Token': window.csrfToken,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        tags,
-      }),
-      credentials: 'same-origin',
-    });
+    const tagArr = tags.split(',');
+    if (title.length && tags.length) {
+      fetch('/reading_collections', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'X-CSRF-Token': window.csrfToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          tags: tagArr,
+        }),
+        credentials: 'same-origin',
+      });
+      window.location.href = '/readinglist';
+    } else {
+      console.log('Please fill out fields');
+    }
   };
 
   render() {
-    const { tagList } = this.state;
+    const { title, tagList } = this.state;
 
     return (
-      <div className="articleformcontainer">
-        <section className="articleform">
-          <form className="articleform__form">
-            <h1>Add your new collection</h1>
-            <input
-              type="text"
-              name="title"
-              placeholder="Title..."
-              onChange={this.handleChange}
-              className="articleform__title"
-              required
-            />
-            <Tags
-              defaultValue={tagList}
-              onInput={linkState(this, 'tagList')}
-              maxTags={4}
-              autoComplete="off"
-              classPrefix="articleform"
-            />
-            {/* <h4>{`${this.state.error}`}</h4> */}
-            <a
-              className="articleform__buttons--publish collection-btn"
-              type="button"
-              href="readinglist"
-            >
-              CREATE COLLECTION
-            </a>
-          </form>
-        </section>
-      </div>
+      <section className="articleform">
+        <form className="articleform__form">
+          <h1>Add your new collection</h1>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title..."
+            onChange={this.handleChange}
+            className="articleform__title"
+            required
+          />
+          <Tags
+            defaultValue={tagList}
+            onInput={linkState(this, 'tagList')}
+            maxTags={4}
+            autoComplete="off"
+            classPrefix="articleform"
+          />
+          {/* <h4>{`${this.state.error}`}</h4> */}
+          <button
+            className="articleform__buttons--publish button collection-btn"
+            type="button"
+            onClick={e => this.submitCollection(e, title, tagList)}
+          >
+            CREATE COLLECTION
+          </button>
+        </form>
+      </section>
     );
   }
 }
