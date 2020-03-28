b5b773aaffb98e3f9474c46e848a364f6c05499f
Linda Le <yueling1222@gmail.com>
2020-03-28 01:54:16 -0600

    Delete email_controller.rb
    This file needs to be deleted.

--- app/controllers/email_controller.rb
+++
@@ -1,6 +0,0 @@
-class EmailController < ApplicationController
-  def index
-    article = EmailDigest.send_daily_digest_email
-    DailyDigestMailer.daily_digest_email(current_user, article).deliver_now
-  end
-end
