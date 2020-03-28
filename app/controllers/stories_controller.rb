b47c7ec036bc03fb2339b7f07cfd0f3f45044b52
Darren Campbell <darren2802@gmail.com>
2020-03-28 15:58:59 -0600

    Refactors youtube_videos method

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -254,28 +254,25 @@ class StoriesController < ApplicationController
   end
 
   def youtube_videos(tags)
-    # tag = tags.split(",")[0]
-    # videos = GoogleService.youtube_videos(tag)
-    # selected_vids = []
-    # 3.times do
-    #   video = videos.pop
-    #   selected_vids << YoutubeVideo.new(video)
-    # end
-    # selected_vids
     videos = GoogleService.youtube_videos(tags)
     videos.delete_if { |video| video[:id][:videoId].nil? }
-    most_relevant = videos[0..2]
-    sorted = videos.sort_by { |video| video[:snippet][:publishedAt] }
-    most_recent = sorted.reverse![0..2]
-    combined = most_relevant.concat(most_recent)
-    ids_combined = combined.map { |video| video[:id][:videoId] }
-    id_string = ids_combined.join(",")
+    videos_abridged = videos_recent_relevant(videos, 3, 3)
+
+    video_ids = videos_abridged.map { |video| video[:id][:videoId] }
+    id_string = video_ids.join(",")
     video_data = GoogleService.video_data(id_string)
 
-    combined.map do |video|
+    videos_abridged.map do |video|
       statistics = video_data.detect { |video_d| video_d[:id] == video[:id][:videoId] }
       YoutubeVideo.new(video, statistics)
     end
+  end
+
+  def videos_recent_relevant(videos, nr_relevant, nr_recent)
+    most_relevant = videos[0..nr_relevant]
+    sorted = videos.sort_by { |video| video[:snippet][:publishedAt] }
+    most_recent = sorted.reverse[0..nr_recent]
+    most_relevant.concat(most_recent)
   end
 
   def permission_denied?
