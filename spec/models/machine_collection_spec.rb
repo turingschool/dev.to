306e13a373b849a73afa6e952d084cf5c0ae4926
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-28 11:48:57 -0600

    Add test for returning articles from past 7 days that are tagged with the tag on the collection

--- spec/models/machine_collection_spec.rb
+++ spec/models/machine_collection_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe MachineCollection, type: :model do
       expect(machine_collection.slug).not_to eq(nil)
     end
   end
+
+  describe "articles_past_seven_days" do
+    let(:user) { create(:user) }
+
+    it "returns a collection of articles that are tagged with the collection's tag from the past seven days" do
+      javascript_machine_collection = described_class.create(title: "Javascript Collection", user_id: user.id, cached_tag_list: "javascript")
+      ruby_machine_collection = described_class.create(title: "Ruby Articles Collection", user_id: user.id, cached_tag_list: "ruby")
+
+      eight_days_ago = Time.zone.today - 8.days
+
+      create_list(:article, 3, tags: "discuss, javascript, security")
+      create_list(:article, 2, tags: "react, ruby")
+      create_list(:article, 2, tags: "javascript, computerscience, beginners, ruby")
+      create_list(:article, 3, tags: "javascript", created_at: eight_days_ago)
+      create_list(:article, 3, tags: "ruby", created_at: eight_days_ago)
+
+      expect(javascript_machine_collection.articles_past_seven_days.length).to eq(5)
+      expect(ruby_machine_collection.articles_past_seven_days.length).to eq(4)
+    end
+  end
 end
