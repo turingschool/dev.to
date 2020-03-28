5019d5dfec810bac1e8af736b8b3860a04541eee
Rachel Lew <rlew07@gmail.com>
2020-03-28 14:10:38 -0600

    Refactor to test for order of articles by page view count

--- spec/user_views_a_collection_show_spec.rb
+++ spec/user_views_a_collection_show_spec.rb
@@ -12,16 +12,20 @@ RSpec.describe "Collection show page", type: :system do
     three_days_ago = Time.zone.today - 3.days
     ten_days_ago = Time.zone.today - 10.days
     machine_collection = create(:machine_collection, user: user, cached_tag_list: "javascript", title: "Best of JavaScript")
-    valid_article_1 = create(:article, tags: "javascript", created_at: three_days_ago)
-    valid_article_2 = create(:article, tags: "javascript", created_at: three_days_ago)
+    valid_article_1 = create(:article, tags: "javascript", created_at: three_days_ago, page_views_count: 5)
+    valid_article_2 = create(:article, tags: "javascript", created_at: three_days_ago, page_views_count: 15)
+    valid_article_3 = create(:article, tags: "javascript", created_at: three_days_ago, page_views_count: 0)
     invalid_article = create(:article, tags: "javascript", created_at: ten_days_ago)
-    
+
     visit "/#{user.id}/collections/#{machine_collection.slug}"
 
-    expect(page).to have_css(".machine_collection_articles")
     expect(page).to have_content("Articles in #{machine_collection.title}")
-    expect(page).to have_content("#{valid_article_1.title}")
-    expect(page).to have_content("#{valid_article_2.title}")
-    expect(page).to_not have_content("#{invalid_article.title}")
+
+    within ".machine_collection_articles" do
+      expect(page.all('li')[0]).to have_content(valid_article_2.title)
+      expect(page.all('li')[1]).to have_content(valid_article_1.title)
+      expect(page.all('li')[2]).to have_content(valid_article_3.title)
+      expect(page).to_not have_content("#{invalid_article.title}")
+    end
   end
 end
