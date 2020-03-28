6c8016382bf8df64f8fe365df3ddbd1fd4ca63e4
Linda Le <yueling1222@gmail.com>
2020-03-28 13:27:37 -0600

    Add missing routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,8 +114,8 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: "587",
     authentication: :plain,
-    user_name: ENV["SENDGRID_USERNAME_ACCEL"],
-    password: ENV["SENDGRID_PASSWORD_ACCEL"],
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
     domain: ENV["APP_DOMAIN"],
     enable_starttls_auto: true
   }
