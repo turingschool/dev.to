dd57602d76ac66cd233005a63be14d9eeae4c3d3
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-28 15:37:33 -0600

    Fix unsubscribe token

--- app/mailers/daily_dev_mailer.rb
+++ app/mailers/daily_dev_mailer.rb
@@ -4,7 +4,7 @@ class DailyDevMailer < ApplicationMailer
   def daily_dev_email(user, article)
     @user = user
     @article = article
-    @unsubscribe = generate_unsubscribe_token(id, :email_daily_dev)
+    @unsubscribe = generate_unsubscribe_token(@user.id, :email_daily_dev)
     subject = "Your Daily Dev"
     mail(to: @user.email, subject: subject)
   end
