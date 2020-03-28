1bfe2d4a81855318306c3829c6e127a17ff991cc
Carleigh Crockett <capleugh@gmail.com>
2020-03-28 01:09:09 -0600

    Create DailyDigestEmail class to handle sending daily digest email
    Co-authored-by: Linda Le <54052410+linda-le1@users.noreply.github.com>

---
+++ app/labor/daily_digest_email.rb
@@ -0,0 +1,27 @@
+class DailyDigestEmail
+  def self.send_daily_digest_email(users = [])
+    new(users).send_daily_digest_email
+  end
+
+  def initialize(users = [])
+    @users = users.empty? ? get_users : users
+  end
+
+  def send_daily_digest_email
+    @users.find_each do |user|
+      article = EmailLogic.new(user).analyze_daily_email
+
+      begin
+        DailyDigestMailer.daily_digest_email(user, article).deliver if user.email_daily_digest_notifications == true
+      rescue StandardError => e
+        Rails.logger.error("Email issue: #{e}")
+      end
+    end
+  end
+
+  private
+
+  def get_users
+    User.where(email_daily_digest_notifications: true).where.not(email: "")
+  end
+end
