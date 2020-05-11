e3e59c0284ec54d0d9f461cf301bfad89aebe71c
Meghan Stovall <meghanstovall15@augustana.edu>
2020-05-11 14:07:21 -0600

    Create model tests for the machine collection table
    Co-Authored-By: Alex G <agallant1201@gmail.com>
    Co-Authored-By: Chris <cjrpostma@gmail.com>

---
+++ spec/models/machine_collection_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe MachineCollection, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of :title }
+  end
+
+  describe "relationships" do
+    it { is_expected.to have_many :tags }
+    it { is_expected.to belong_to :user }
+  end
+end
