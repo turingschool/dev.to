049c0d5e4e459492c5e16c628d0a04fb48101220
Nathan Keller <keller.nathan@gmail.com>
2020-05-12 13:05:35 -0600

    Add before action to test and sign in a user

--- spec/requests/tagcollections_spec.rb
+++ spec/requests/tagcollections_spec.rb
@@ -1,15 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Tagcollections", type: :request do
-  describe "GET /mycollections" do
-    it "successfully routes to /tagcollections" do
-      get "/tagcollections"
-
-      expect(response).to have_http_status(:ok)
-    end
-
-    # xit "navigates by links to my collections" do
-    #
-    # end
+  before do
+    user = create(:user)
+    # tagcollection = @user.tagcollections.create(name: "All the Ruby", tag_list: ['ruby'])
+    sign_in user
   end
+
+  it "successfully routes to /tagcollections" do
+    get "/tagcollections"
+
+    expect(response).to have_http_status(:ok)
+  end
+
+  # xit "navigates by links to my collections" do
+  #
+  # end
 end
