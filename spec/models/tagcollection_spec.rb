d8ac021420f9c3e19b59c14064060b9b11218056
Nathan Keller <keller.nathan@gmail.com>
2020-05-12 17:29:48 -0600

    add tagcollection model spec

---
+++ spec/models/tagcollection_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Tagcollection, type: :model do
+  describe "validations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to validate_presence_of(:name) }
+
+    acts_as_taggable_on :tags
+  end
+end
