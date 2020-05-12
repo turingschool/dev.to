1cfa22d6ef6887201421bc3f5a3d93dba4aa7576
Pierce Alworth <pbalworth@gmail.com>
2020-05-12 16:42:00 -0500

    Update the test and uncomment. Can hit controller, route is working

--- spec/requests/machine_collections/machine_collection_index_spec.rb
+++ spec/requests/machine_collections/machine_collection_index_spec.rb
@@ -1,21 +1,26 @@
-# require "rails_helper"
-#
-# RSpec.describe "Machine Collections", type: :request do
-#   let(:user) { create(:user) }
-#   let(:tag1)  { create(:tag) }
-#   let(:tag2)  { create(:tag) }
-#
-#   describe "get index page machine_collections/index" do
-#     it "returns rss+xml content" do
-#       tags = [tag1, tag2]
-#       require "pry"; binding.pry
-#       x = user.machine_collections.create!(
-#         title: 'Mountain Shelter',
-#         tags: tags
-#       )
-#       get "/feed"
-#       expect(response.status).to eq(200)
-#       expect(response.content_type).to eq("application/rss+xml")
-#     end
-#   end
-# end
+require "rails_helper"
+
+RSpec.describe "Machine Collections", type: :request do
+  let(:user) { create(:user) }
+  let(:tag1)  { create(:tag) }
+  let(:tag2)  { create(:tag) }
+
+
+  describe "get index page machine_collections/index" do
+    it "returns rss+xml content" do
+      sign_in user
+      create(:article, tags: %w[ruby preact rails])
+      create(:article, tags: %w[ruby preact rails])
+      create(:article, tags: %w[rails preact heroku])
+      tags = [tag1, tag2]
+      # x = user.machine_collections.create!(
+      #   title: 'Mountain Shelter',
+      #   tags: %w[ruby preact rails]
+      # )
+      get "/machinecollections"
+      require "pry"; binding.pry
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq("application/json")
+    end
+  end
+end
