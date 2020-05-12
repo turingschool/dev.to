46f82194693a44b37e2bc872af74467f7939dbed
Nathan Keller <keller.nathan@gmail.com>
2020-05-12 14:45:22 -0600

    add articles to tagcollection request spec

--- app/controllers/tagcollections_controller.rb
+++ app/controllers/tagcollections_controller.rb
@@ -1,5 +1,5 @@
 class TagcollectionsController < ApplicationController
   def index
-    # @tagcollection = current_user.tagcollections.all
+    # @tagcollection = current_user.tagcollections.all.to_json
   end
 end
--- spec/requests/tagcollections_spec.rb
+++ spec/requests/tagcollections_spec.rb
@@ -2,6 +2,12 @@ require "rails_helper"
 
 RSpec.describe "Tagcollections", type: :request do
   before do
+    create(:article, tags: %w[ruby preact rails])
+    create(:article, tags: %w[ruby preact rails])
+    create(:article, tags: %w[rails preact heroku])
+    create(:article, tags: %w[javascript rails sql])
+    create(:article, tags: %w[rails preact sql])
+    create(:article, tags: %w[javascript preact sql])
     user = create(:user)
     # tagcollection = @user.tagcollections.create(name: "All the Ruby", tag_list: ['ruby'])
     sign_in user
