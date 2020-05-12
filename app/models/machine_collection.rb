aab26b4f077d6324dba13bb4a03fb24c1721f02d
Pierce Alworth <pbalworth@gmail.com>
2020-05-12 15:39:32 -0500

    Add article-collection joins relation to machine collection

--- app/models/machine_collection.rb
+++ app/models/machine_collection.rb
@@ -1,7 +1,10 @@
 class MachineCollection < ApplicationRecord
   belongs_to :user
+  has_many :machine_collection_articles, dependent: :destroy
+  has_many :articles, through: :machine_collection_articles
 
   acts_as_taggable_on :tags
+  resourcify
 
   validates :title, :tags, presence: true
 end
