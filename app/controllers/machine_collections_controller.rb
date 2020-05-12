a37e666cff4cf434bb5f85e1e29b0fda63737e1a
Pierce Alworth <pbalworth@gmail.com>
2020-05-12 16:38:36 -0500

    Add machine collecitons controller

---
+++ app/controllers/machine_collections_controller.rb
@@ -0,0 +1,14 @@
+class MachineCollectionsController < ApplicationController
+
+  before_action :authenticate_user!, except: %i[feed new]
+
+  def index
+    require "pry"; binding.pry
+    render :json
+  end
+
+  def machine_collection_params
+   params.require(:user).permit(:name, :tag_list) 
+ end
+
+end
