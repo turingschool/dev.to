a7fb082c6ae2050ba1f668ca9c3b65c82aa21e1f
Meghan Stovall <meghanstovall15@augustana.edu>
2020-05-12 14:42:19 -0600

    Created the one to many relationship between user and machine collections
    Co-Authored-By: Alex G <agallant1201@gmail.com>
    Co-Authored-By: Chris <cjrpostma@gmail.com>

--- db/migrate/20200511195300_create_machine_collection.rb
+++ db/migrate/20200511195300_create_machine_collection.rb
@@ -2,7 +2,7 @@ class CreateMachineCollection < ActiveRecord::Migration[5.2]
   def change
     create_table :machine_collections do |t|
       t.string :title
-      t.references :user, index: { unique: true }
+      t.references :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_195300) do
+ActiveRecord::Schema.define(version: 2020_05_12_190029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -528,7 +528,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_195300) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_machine_collections_on_user_id", unique: true
+    t.index ["user_id"], name: "index_machine_collections_on_user_id"
   end
 
   create_table "mentions", id: :serial, force: :cascade do |t|
