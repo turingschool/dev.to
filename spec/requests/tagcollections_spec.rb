5a4f8e89c1ac1a06908bf1388ba89b2b42a902dc
Nathan Keller <keller.nathan@gmail.com>
2020-05-12 12:13:54 -0600

    adjust tagcollection index spec and create index view

--- spec/requests/tagcollections_spec.rb
+++ spec/requests/tagcollections_spec.rb
@@ -1,16 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Tagcollections", type: :request do
-  let(:user) { create(:user) }
-  let(:separate_user) { create(:user) }
-  let(:article) { create(:article, user_id: user.id) }
-  let(:reaction) { create(:reaction, reactable: article, user_id: user.id) }
-  let(:unauthorized_reaction) { create(:reaction, reactable: article, user_id: separate_user.id) }
-
-  before do
-    sign_in user
-  end
-
   describe "GET /mycollections" do
     it "successfully routes to /tagcollections" do
       get "/tagcollections"
@@ -18,7 +8,8 @@ RSpec.describe "Tagcollections", type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    xit "navigates by links to my collections" do
-    end
+    # xit "navigates by links to my collections" do
+    #
+    # end
   end
 end
