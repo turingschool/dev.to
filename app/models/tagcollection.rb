9a22b6b699726263ff8746033fa6d44651c6041c
Nathan Keller <keller.nathan@gmail.com>
2020-05-13 07:12:49 -0600

    update tagcollection model tobelongs to user, presence true

--- app/models/tagcollection.rb
+++ app/models/tagcollection.rb
@@ -1,6 +1,6 @@
 class Tagcollection < ApplicationRecord
   validates :name, presence: true
-  belongs_to :user, optional: true
+  belongs_to :user, presence: true
 
   acts_as_taggable_on :tags
 end
