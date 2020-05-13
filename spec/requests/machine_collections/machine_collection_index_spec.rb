fd0f4396c1c756ba0d4900b2f5be84c2ed9bb107
Pierce Alworth <pbalworth@gmail.com>
2020-05-12 21:06:51 -0500

    Clean up debugging tools

--- spec/requests/machine_collections/machine_collection_index_spec.rb
+++ spec/requests/machine_collections/machine_collection_index_spec.rb
@@ -7,18 +7,14 @@ RSpec.describe "Machine Collections", type: :request do
 
 
   describe "get index page machine_collections/index" do
-    it "returns rss+xml content" do
+    it "User can visit thier machine colleciton index" do
       sign_in user
-      create(:article, tags: %w[ruby preact rails])
-      create(:article, tags: %w[ruby preact rails])
-      create(:article, tags: %w[rails preact heroku])
-      tags = [tag1, tag2]
-      # x = user.machine_collections.create!(
-      #   title: 'Mountain Shelter',
-      #   tags: %w[ruby preact rails]
-      # )
+      user.machine_collections.create!(title: "Ruby collection")
+      create(:article, tags: %w[ruby java rails])
+      create(:article, tags: %w[ruby javascript rails])
+      create(:article, tags: %w[rails preact])
+      user.machine_collections.first.articles << Article.all
       get "/machinecollections"
-      require "pry"; binding.pry
       expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
     end
