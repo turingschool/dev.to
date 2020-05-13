5540989862384abb3b472c3bcaca3a1ca48027c4
Pierce Alworth <pbalworth@gmail.com>
2020-05-12 21:03:15 -0500

    Add index logic and remove render json method

--- app/controllers/machine_collections_controller.rb
+++ app/controllers/machine_collections_controller.rb
@@ -3,12 +3,12 @@ class MachineCollectionsController < ApplicationController
   before_action :authenticate_user!, except: %i[feed new]
 
   def index
-    require "pry"; binding.pry
-    render :json
+    @machine_collections = current_user.machine_collections
   end
 
+private
   def machine_collection_params
-   params.require(:user).permit(:name, :tag_list)
+   params.require(:user).permit(:name, :tag_list, :user_id)
   end
 
 end
