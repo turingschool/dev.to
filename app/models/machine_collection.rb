d1ec24cbce20a54273ef6c2733b8e36b3cd50957
Pierce Alworth <pbalworth@gmail.com>
2020-05-12 21:05:30 -0500

    Remove tag validation

--- app/models/machine_collection.rb
+++ app/models/machine_collection.rb
@@ -1,10 +1,11 @@
 class MachineCollection < ApplicationRecord
-  belongs_to :user
-  has_many :machine_collection_articles, dependent: :destroy
-  has_many :articles, through: :machine_collection_articles
+ belongs_to :user
+ has_many :machine_collection_articles, dependent: :destroy
+ has_many :articles, through: :machine_collection_articles
 
-  acts_as_taggable_on :tags
-  resourcify
+ acts_as_taggable_on :tags
+ resourcify
 
-  validates :title, :tags, presence: true
+ validates_presence_of :title
+
 end
