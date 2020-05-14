a5dfa487acf8e6db6df7ed237c877468acb3b10e
Alex Gallant <agallant1201@gmail.com>
2020-05-14 15:55:46 -0600

    Add collectionList folder with collection_list.jsx
    Co-Authored-By: Meghan Stovall <meghanstovall@users.noreply.github.com>
    Co-Authored-By: Chris Postma <cjrpostma@gmail.com>

---
+++ app/javascript/collectionList/collectionList.jsx
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact';
+
+export class CollectionList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <section className="collection-list-container" />;
+  }
+}
