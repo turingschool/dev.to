7c0c73fb2fb61562ab71f74cda47b28a39eb2ca8
Pierce Alworth <pbalworth@gmail.com>
2020-05-14 15:57:11 -0500

    Refactor current_user to be private method. Add logic for show method

--- app/controllers/machine_collections_controller.rb
+++ app/controllers/machine_collections_controller.rb
@@ -3,12 +3,22 @@ class MachineCollectionsController < ApplicationController
   before_action :authenticate_user!, except: %i[feed new]
 
   def index
-    @machine_collections = current_user.machine_collections
+    @machine_collections = logged_in_user.machine_collections
+  end
+
+  def show
+    @machine_collection = logged_in_user.machine_collections.find(params[:id])
   end
 
 private
+
   def machine_collection_params
-   params.require(:user).permit(:name, :tag_list, :user_id)
+    allowed_params = %i[title user_id tag_list]
+    params.require(:machine_collections).permit(allowed_params)
+  end
+
+  def logged_in_user
+    current_user
   end
 
 end
