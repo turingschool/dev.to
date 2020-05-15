5d451367cfadda3d0e435b2e46e7447dbd42f632
Elom Amouzou <elom1990@yahoo.com>
2020-05-15 13:38:09 -0600

    refactor articles spec to account for descriptions and title params

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Articles Youtube Section" do
 
-  describe "Youtube Video Section on Articles Show View", :vcr do
+  describe "Youtube Video Section on Articles Show View" do
 
     it "can show a heading and description of related videos." do
       user = create(:user)
@@ -10,17 +10,22 @@ RSpec.describe "Articles Youtube Section" do
       article_2 = create(:article, user_id: user.id)
       last_article = create(:article, user_id: user.id)
       tags = last_article.cached_tag_list
+      description = last_article.description
+      title = last_article.title
 
       visit "/#{user.username}"
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_2.title)
       expect(page).to have_content(last_article.title)
-      
+
       click_on(last_article.title)
 
       expect(page).to have_content('Related Videos')
-      expect(page).to have_content("The videos below are based on the following tags: #{tags}.")
+      expect(page).to have_content("The videos below are based on the following.")
+      expect(page).to have_content("Tags: #{tags}")
+      expect(page).to have_content("Description: #{description}")
+      expect(page).to have_content("Title: #{title}")
     end
 
   end
