7df859e893efe44bcb3396eb7d7f5020b7e644dc
monstaro <codysmith2814@gmail.com>
2020-05-14 18:59:19 -0600

    Removes comments

--- app/controllers/collection_items_controller.rb
+++ app/controllers/collection_items_controller.rb
@@ -1,27 +1,16 @@
 class CollectionListItemsController < ApplicationController
-  # creating a new class, ReadingListItemsController, which inherets from ApplicationController
   def index
-    # creates method on class called index
     @collection_list_items_index = true
-    # setting instance variable readling_list_items_index to true
     set_view
-    # runs set_view method seen below
     generate_algolia_search_key
-    # runs method seen below
   end
-  # finishes index method
 
   def update
     @reaction = Reaction.find(params[:id])
-    # creates instance variable that can find a reaction based on its id from the Reaction model
     not_authorized if @reaction.user_id != session_current_user_id
-    # this line will block the action if the user id associated in the session is different from the user id recieved from the reaction variable
     @reaction.status = params[:current_status] == "archived" ? "valid" : "archived"
-    # determines if a reaction has been archived or valid
     @reaction.save
-    # run the save method on reaction (is this a built in method)
     head :ok
-    # returns a response header to the browser
   end
 
   private
