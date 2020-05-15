47144b03b2277b40ed478b3b8180b73f50705425
Maria Ronauli <mronauli@gmail.com>
2020-05-15 16:19:19 -0600

    add suggested articles method

--- app/models/machine_collection.rb
+++ app/models/machine_collection.rb
@@ -1,10 +1,14 @@
 class MachineCollection < ApplicationRecord
- belongs_to :user
- has_many :machine_collection_articles, dependent: :destroy
- has_many :articles, through: :machine_collection_articles
+  belongs_to :user
+  has_many :machine_collection_articles, dependent: :destroy
+  has_many :articles, through: :machine_collection_articles
 
- acts_as_taggable_on :tags
- resourcify
+  acts_as_taggable_on :tags
+  resourcify
 
- validates_presence_of :title, :tag_list
+  validates :title, :tag_list, presence: true
+
+  def suggested_articles
+    ArticleSuggester.new(nil, self).machine_articles(max: 10)
+  end
 end
