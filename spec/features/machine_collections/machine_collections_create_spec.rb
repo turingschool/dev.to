7c6a21d233c710484f19b40be607fc74d6dfa90f
Pierce Alworth <pbalworth@gmail.com>
2020-05-15 12:16:34 -0500

    Add spec for create request

--- spec/features/machine_collections/machine_collections_create_spec.rb
+++ spec/features/machine_collections/machine_collections_create_spec.rb
@@ -4,8 +4,10 @@ describe "Machine Collections" do
   let(:user) { create(:user) }
   let(:tag1) { create(:tag) }
   describe "render machine collection new page" do
-    it "can get one of its collections" do
+    it "can visit the new page" do
       visit "/machinecollections/new"
+      expect(current_path).to eq( "/machinecollections/new")
     end
+    
   end
 end
